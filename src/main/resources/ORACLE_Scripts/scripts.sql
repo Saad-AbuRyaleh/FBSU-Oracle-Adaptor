ee3cfede6a7966ae05873970c00a4014d2fd1cfc
Saad-AbuRyaleh <saburyaleh@invoiceq.com>
2024-05-05 13:25:20 +0300

    Required Scripts

--- src/main/resources/ORACLE_Scripts/scripts.sql
+++ src/main/resources/ORACLE_Scripts/scripts.sql
@@ -1,10 +1,10 @@
-CREATE TABLE log_event(
-  event_type VARCHAR2(255),
-  event_status VARCHAR2(255),
-  invoice_number VARCHAR2(255),
-  trace_id VARCHAR2(255),
-  request VARCHAR2(2000),
-  response VARCHAR2(2000),
-  formatted_message VARCHAR2(2000),
-  event_date_time VARCHAR2(255)
+CREATE TABLE "NODE_LOGGER"
+(    "ID" NUMBER,
+     "LAST_ACTIVE_ON" TIMESTAMP (6),
+     "SERVICE_CODE" VARCHAR2(255 BYTE)
 );
+ALTER TABLE "INVOICEQ_ADAPTER"."NODE_LOGGER" ADD PRIMARY KEY ("ID");
+
+CREATE SEQUENCE node_sequence;
+
+CREATE SEQUENCE attachment_Seq;
